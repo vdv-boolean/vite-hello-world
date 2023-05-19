a38bc71cb7389e576d36cd5a00f7a72b6db3b26e
vincenzodivita <divitavincenzo@icloud.com>
2023-05-19 09:55:44 +0200

    Remove Vite demo code and Add Hello World msg

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,13 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  
+ <HelloWorld msg="La mia prima app con Vite!" />
+
 </template>
 
 <style scoped>
-.logo {
+.logo { 
   height: 6em;
   padding: 1.5em;
   will-change: filter;

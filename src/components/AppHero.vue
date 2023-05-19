74f20edb3bd95b06b65b012c484a2714520ac43d
vincenzodivita <divitavincenzo@icloud.com>
2023-05-19 10:01:30 +0200

    Add parag. as jumbotron

---
+++ src/components/AppHero.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+ 
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat aliquid, veritatis tempora autem reprehenderit voluptatibus sapiente recusandae veniam consequuntur quos consectetur blanditiis unde nisi voluptatem quis hic molestias optio mollitia.</p>
+  
+
+  
+</template>
+
+<style scoped>
+
+</style>

c85e43448adbc75e0e0715f6be22dea7ecee8056
vincenzodivita <divitavincenzo@icloud.com>
2023-05-19 10:01:47 +0200

    Add AppHero component to App

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import AppTitle from './components/AppTitle.vue'
+import AppHero from './components/AppHero.vue'
 </script>
 
 <template>
 
  <AppTitle msg="La mia prima app con Vite!" />
+ <AppHero />
 
 </template>
 
